54a28c46448a78917d13645e3b6fd4c047719f94
MaxBorovique <max.borovique@gmail.com>
2025-09-17 18:43:33 +0300

    added new functionality to appwrite and added custom hook for data fetching. implemeted search screen main logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,8 +15,7 @@ import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
   const {user} = useAuthStore();
-
-  console.log('user:', user);
+  
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
